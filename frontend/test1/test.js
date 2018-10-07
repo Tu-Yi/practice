dde148cf87a6d989d1be0230354e31ff2e551dce
niliv <yk.hm.eli@gmail.com>
2018-10-07 15:13:30 +0800

    ife911

--- frontend/test1/test.js
+++ frontend/test1/test.js
@@ -1,11 +1,9 @@
-var arr = [10, 20, 1, 2];
-arr.sort(function (x, y) {
-    if (x < y) {
-        return -1;
-    }
-    if (x > y) {
-        return 1;
-    }
-    return 0;
-});
-console.log(arr); // [1, 2, 10, 20]
+
+
+var s = 'Hello';
+
+function greet(name) {
+    console.log(s + ', ' + name + '!');
+}
+
+module.exports = greet;
