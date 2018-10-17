043932247efcc348af18dd3e234f67cb0f88dff4
niliv <yk.hm.eli@gmail.com>
2018-10-17 15:54:39 +0800

    test

--- frontend/test1/main.js
+++ frontend/test1/main.js
@@ -1,20 +1,20 @@
-'use strict';
+function doAction(action) {
+  var actions = {
+    'hack': function () {
+      return 'hack';
+    },
+    'slash': function () {
+      return 'slash';
+    },
+    'run': function () {
+      return 'run';
+    }
+  };
 
-// 导入http模块:
-var http = require('http');
+  if (typeof actions[action] !== 'function') {
+    throw new Error('Invalid action.');
+  }
 
-// 创建http server，并传入回调函数:
-var server = http.createServer(function (request, response) {
-    // 回调函数接收request和response对象,
-    // 获得HTTP请求的method和url:
-    console.log(request.method + ': ' + request.url);
-    // 将HTTP响应200写入response, 同时设置Content-Type: text/html:
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    // 将HTTP响应的HTML内容写入response:
-    response.end('<h1>Hello world!</h1>');
-});
-
-// 让服务器监听8080端口:
-server.listen(8080);
-
-console.log('Server is running at http://127.0.0.1:8080/');
+  return actions[action]();
+}
+console.log(doAction('yk'));
--- frontend/test1/test.js
+++ frontend/test1/test.js
@@ -1,9 +1,1 @@
-
-
-var s = 'Hello';
-
-function greet(name) {
-    console.log(s + ', ' + name + '!');
-}
-
-module.exports = greet;
+console.log(1);
