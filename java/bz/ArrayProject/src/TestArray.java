1e30d337bfa5fa7de50acae6a480f46678b41546
kun.yuan <yk_niliv@qq.com>
2019-03-14 11:51:02 +0800

    all

--- java/bz/ArrayProject/src/TestArray.java
+++ java/bz/ArrayProject/src/TestArray.java
@@ -2,31 +2,41 @@
 public class TestArray {
 	public static void main(String[] args) {
 		
-		//定义二维数组的三种方式
-		int[][] arr = {{1,2},{3,4,5},{6}};
-		int[][] brr = new int[3][];
-		int[][] crr = new int[3][3];
+//		//定义二维数组的三种方式
+//		int[][] arr = {{1,2},{3,4,5},{6}};
+//		int[][] brr = new int[3][];
+//		int[][] crr = new int[3][3];
+//		
+//		//循环二维数组
+//		for(int i=0;i<arr.length;i++) {
+//			for(int j=0;j<arr[i].length;j++) {
+//				System.out.print(arr[i][j] + "\t");
+//			}
+//			System.out.println();
+//		}
+//		
+//		for(int[] a : arr) {
+//			for(int i : a) {
+//				System.out.print(i+"\t");
+//			}
+//			System.out.println();
+//		}
+//		
+//		for(int i=0;i<arr.length;i++) {
+//			for(int j : arr[i]) {
+//				System.out.print(j+"\t");
+//			}
+//			System.out.println();
+//		}
 		
-		//循环二维数组
-		for(int i=0;i<arr.length;i++) {
-			for(int j=0;j<arr[i].length;j++) {
-				System.out.print(arr[i][j] + "\t");
+		int[] arr1 = {1,2,3,4,5};
+		int max = arr1[0];
+		for (int i : arr1) {
+			if(max<i) {
+				max = i;
 			}
-			System.out.println();
 		}
+		System.out.println(max);
 		
-		for(int[] a : arr) {
-			for(int i : a) {
-				System.out.print(i+"\t");
-			}
-			System.out.println();
-		}
-		
-		for(int i=0;i<arr.length;i++) {
-			for(int j : arr[i]) {
-				System.out.print(j+"\t");
-			}
-			System.out.println();
-		}
 	}
 }
--- java/bz/ArrayProject/src/TestArrayCopy.java
+++ java/bz/ArrayProject/src/TestArrayCopy.java
@@ -5,7 +5,7 @@ public class TestArrayCopy {
 	public static void main(String[] args) {
 		
 		int [] arr = {1,2,3,4};
-		int [] brr = new int[1];
+		int [] brr = new int[4];
 		
 		//直接赋值 由于是地址传递，所以可以随意完整赋值，长度不影响赋值结果
 		//brr = arr;
@@ -17,7 +17,7 @@ public class TestArrayCopy {
 //		}
 		
 		//使用系统方法 赋值位置和长度不能越界，否则报错
-		//System.arraycopy(arr, 0, brr, 0, 4); 
+		System.arraycopy(arr, 0, brr, 0, 4); 
 		
 		System.out.println(Arrays.toString(arr));
 		System.out.println(Arrays.toString(brr)); 
