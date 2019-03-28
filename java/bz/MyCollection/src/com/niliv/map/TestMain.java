839ab00a8751afabc84ccdf1eb44bf68b0cc78ff
niliv <yk_niliv@qq.com>
2019-03-28 23:11:34 +0800

    a

--- java/bz/MyCollection/src/com/niliv/map/TestMain.java
+++ java/bz/MyCollection/src/com/niliv/map/TestMain.java
@@ -7,10 +7,11 @@ public class TestMain {
 		map.put("one", "旺财");
 		map.put("two", "小强");
 		map.put("three", "百战程序员");
-		System.out.println(map);
-		System.out.println("one"+":"+map.get("one"));
+		System.out.println(map); //[three:百战程序员,one:旺财,two:小强]
+		System.out.println("one"+":"+map.get("one")); //one:旺财
 		Thread.sleep(200);
-		System.out.println(map.remove("two")); 
-		System.out.println(map.get("two"));
+		System.out.println(map.remove("two"));  //小强
+		System.out.println(map.get("two")); //null
+		System.out.println(map); //[three:百战程序员,one:旺财]
 	}
 }
