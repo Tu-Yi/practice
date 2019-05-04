85896907310eec0b5458ea337ea797fe3a56f48f
kun.yuan <yk_niliv@qq.com>
2019-05-04 19:45:46 +0800

    af

--- java/bz/Server_Test/src/com/niliv/servlet/XmlTest.java
+++ java/bz/Server_Test/src/com/niliv/servlet/XmlTest.java
@@ -11,13 +11,13 @@ import org.xml.sax.SAXException;
 
 public class XmlTest {
 	public static void main(String[] args) throws Exception {
-	//1)	创建SAXParserFactory的对象
+
 		SAXParserFactory spf=SAXParserFactory.newInstance();
-	//2)	创建SAXParser对象 (解析器)
+
 		SAXParser parser=spf.newSAXParser();
-	//3)	创建一个DefaultHandler的子类
+
 		WebDeaultHandler handler=new WebDeaultHandler();
-	//4)	调用parse方法
+
 		parser.parse("web.xml", handler);
 		
 		WebContext context = new WebContext(handler.getEntitys(), handler.getMappings());
